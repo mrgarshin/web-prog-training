142b26098c21ff0a8e7f0685fed0911c64d226ad
Sergey Garshin <74963806+mrgarshin@users.noreply.github.com>
2020-12-03 18:13:01 +0300

    Create db.php

---
+++ php/reg and auth homework/components/db.php
@@ -0,0 +1,11 @@
+<?
+$dbhost = 
+$dbuser = 
+$dbpass = 
+$dbname = 
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$mysqli->set_charset("utf-8");
+
+if ($mysqli->connect_error) {
+  die("Не удалось подключиться к БД ".$mysqli->connect_error);
+} 
